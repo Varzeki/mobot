1583f5340abc2452e1e5b10965f07dc7a4ac505a
passwords <ruijie.shao@gmail.com>
2016-12-14 12:17:10 +0000

    Added quests, betting

--- mobot.rb
+++ mobot.rb
@@ -9,7 +9,7 @@ config = YAML.load_file("config.yaml")
 
 #Global database accessible by all threads, technically a problem, but honestly what are the chances?
 $members = []
-
+$quests = []
 
 #Main bot definition
 mobot = Cinch::Bot.new do
@@ -25,11 +25,69 @@ mobot = Cinch::Bot.new do
         db.write(Marshal.dump(cont))
         db.close
     end
+    def update_quests(cont)
+        db = File.open('./quests', 'w')
+        db.write(Marshal.dump(cont))
+        db.close
+    end
 
+    class Quest
+        attr_accessor :name, :type, :reward, :start, :success, :failure
+
+        def initialize(name, type, reward, start, success, failure)
+            @name = name
+            @type = type
+            @reward = reward
+            @start = start
+            @success = success
+            @failure = failure
+        end
+
+        def attempt(stats)
+            responses = [@start]
+            if @type == "DEX"
+                if rand() > 0.3
+                    responses.push(@success)
+                    responses.push(@reward + ((stats[0] * 7) + (stats[3] * 2)))
+                else
+                    responses.push(@failure)
+                    responses.push(0 - ((@reward).round - stats[3]))
+                end
+            end
+            if @type == "STR"
+                if rand() > 0.3
+                    responses.push(@success)
+                    responses.push(@reward + ((stats[1] * 7) + (stats[3] * 2)))
+                else
+                    responses.push(@failure)
+                    responses.push(0 - ((@reward).round - stats[3]))
+                end
+            end
+            if @type == "INT"
+                if rand() > 0.3
+                    responses.push(@success)
+                    responses.push(@reward + ((stats[2] * 7) + (stats[3] * 2)))
+                else
+                    responses.push(@failure)
+                    responses.push(0 - ((@reward).round - stats[3]))
+                end
+            end
+            if @type == "LCK"
+                if rand() > 0.3
+                    responses.push(@success)
+                    responses.push(@reward + (stats[3] * 9))
+                else
+                    responses.push(@failure)
+                    responses.push(0 - ((@reward).round - stats[3]))
+                end
+            end
+            responses
+        end
+    end
 
     #Class for each user
     class Member
-        attr_accessor :name, :coins, :dex, :str, :int, :lck, :pvp
+        attr_accessor :name, :coins, :dex, :str, :int, :lck, :pvp, :quest, :daily
         
 
         #On initialization, only takes name by default
@@ -42,6 +100,7 @@ mobot = Cinch::Bot.new do
 	        @int = int
 	        @lck = lck
 	        @pvp = false
+            @quest = false
         end
 
 
@@ -56,7 +115,7 @@ mobot = Cinch::Bot.new do
         
 
         #Toggles per user daily variable - NOT responsible for threaded timing
-        def daily()
+        def daily_claim()
             if not @daily
                 @daily = true
                 @coins = @coins + 250
@@ -99,6 +158,9 @@ mobot = Cinch::Bot.new do
 	    def daily_reset()
 	        @daily = false
 	    end
+        def quest_reset()
+            @quest = false
+        end
 	    def buy(item, recipient, m)
 	        if item == "kick"
                     if @coins > 999
@@ -197,8 +259,14 @@ mobot = Cinch::Bot.new do
 	}
     end
     
+    $quests.push(Quest.new("Steal a relic", "DEX", 60, "You are tasked with stealing an ancient relic from a temple!", "You walk past a completely stationary boulder and grab the idol before pleasantly strolling home.", "There had to be snakes.")) 
+    $quests.push(Quest.new("Kill wandering goblins", "STR", 40, "You set out on a quest to kill some wandering goblins!", "You defeat the goblins and turn in the bounty without incident.", "You are knocked unconscious fighting and lose some coins!")) 
+    $quests.push(Quest.new("Decipher a scroll", "INT", 50, "You spend the day trying to decipher an alchemical scroll.", "After finally decrypting the eldritch dialect, the scroll turns to gold as you mutter the incantation. Woops.", "You use up some gold coins trying to understand the magic and accidentally end up turning the scroll into lead. Woops.")) 
+    $quests.push(Quest.new("Open lockbox", "LCK", 80, "You find a wooden lockbox while walking through a forest glade!", "It contains a collection of small gem encrusted rings!", "You are arrested by the city guard on the way home for carrying stolen property and end up having to pay bail.")) 
+
     $members.each do |i|
         i.daily_reset
+        i.quest_reset
     end
 
     Timer(300) {
@@ -222,11 +290,13 @@ mobot = Cinch::Bot.new do
 
     on :message, ".daily" do |m|
 	    usr = mobot.get_user(m.user.to_s, $members)
-	    m.reply usr.daily
-	    threads.push Thread.new {
-		    sleep(86400)
-		    usr.daily_reset
-	    }
+        if usr.daily == false
+	        threads.push Thread.new {
+		        sleep(86400)
+		        usr.daily_reset
+	        }
+        end
+	    m.reply usr.daily_claim
 	    mobot.update_db($members)
     end
 
@@ -249,19 +319,47 @@ mobot = Cinch::Bot.new do
     end
 
     on :message, ".pvp" do |m|
-	    m.reply mobot.get_user(m.user.to_s, $members).pvp
+        m.reply mobot.get_user(m.user.to_s, $members).pvp
     end
+
+    on :message, ".quest" do |m|
+        quest = $quests.sample
+        user = mobot.get_user(m.user.to_s, $members)
+        if user.quest == false
+            result = quest.attempt(user.get_stats)
+            reward = result[2]
+            m.reply result[0]
+            m.reply result[1]
+            if (user.coins + reward) < 1
+                neg = user.coins
+                user.coins = 0
+                m.reply "That quest netted you #{neg} coins! You now have 0 coins!"
+            else
+                user.coins = user.coins + reward
+                current = user.coins
+                m.reply "That quest netted you #{reward} coins! You now have #{current} coins!"
+            end
+            user.quest = true
+            mobot.update_db($members)
+            sleep(180)
+            user.quest = false
+        else
+            m.reply "You already went on a quest recently! Take a break for a minute or three."
+        end
+    end
+        
 
     on :message, ".help" do |m|
         User(m.user.to_s).send("Hi! I'm mobot! I allow you to save up coins via playing games with other bots in irc such as UNOBot or Trivia, and eventually spend them to devoice or kick other $members, even if you don't have permission to do so. Try doing '.daily' to get started.")
-	User(m.user.to_s).send('Commands are as follows:')
-	User(m.user.to_s).send('.daily - Claims your daily 250 coins')
-	User(m.user.to_s).send('.coins - Shows your current balance')
-	User(m.user.to_s).send('.purchase {item} {recipient} - Purchases an item')
-	User(m.user.to_s).send('.store - Lists items for purchase')
-	User(m.user.to_s).send('.taytay - Shows current taylorswift balance')
-	User(m.user.to_s).send('.rob - Pay 20 coins to attempt to rob another user')
+	    User(m.user.to_s).send('Commands are as follows:')
+	    User(m.user.to_s).send('.daily - Claims your daily 250 coins')
+	    User(m.user.to_s).send('.coins - Shows your current balance')
+	    User(m.user.to_s).send('.purchase {item} {recipient} - Purchases an item')
+	    User(m.user.to_s).send('.store - Lists items for purchase')
+	    User(m.user.to_s).send('.taytay - Shows current taylorswift balance')
+	    User(m.user.to_s).send('.rob - Pay 20 coins to attempt to rob another user')
         User(m.user.to_s).send('.attr - Shows your current attributes')
+        User(m.user.to_s).send('.quest - Attempt a quest')
         User(m.user.to_s).send('.pvp - Toggles your PvP status')
     end
 
@@ -335,6 +433,33 @@ mobot = Cinch::Bot.new do
 	    recipient = lst[1]
 	    m.reply mobot.get_user(m.user.to_s, $members).buy(item, recipient, m)
 	    mobot.update_db($members)
+    end
+
+    on :message, ".migrate" do |m|
+        m.reply "Migrating database..."
+        new_db = []
+        $members.each do |i|
+            new_db.push(Member.new(i.name, i.coins, i.dex, i.str, i.int, i.lck))
+        end
+        $members = new_db
+        mobot.update_db($members)
+        m.reply "Done!"
+    end
+
+    on :message, /^.bet (.+)/ do |m, arg|
+        lst = arg.split(' ')
+        user = mobot.get_user(m.user.to_s, $members)
+        if lst[0].to_i > 1
+            if rand() > 0.6
+                user.coins = user.coins + lst[0].to_i
+                amount = user.coins
+                m.reply "Congratulations, you won! You now have #{amount} coins!"
+            else
+                user.coins = user.coins - lst[0].to_i
+                amount = user.coins
+                m.reply "You lost! You now have #{amount} coins!"
+            end
+        end
     end
 end
 
